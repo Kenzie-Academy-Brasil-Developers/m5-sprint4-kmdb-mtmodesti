4b5ad79128f189d3c2a00f3cc19873e22be06bf3
mtmodesti <marcelomodesti1988@gmail.com>
2022-06-21 11:34:59 -0300

    feat: super user and login by email done

--- users/models.py
+++ users/models.py
@@ -1,3 +1,4 @@
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from datetime import datetime
@@ -8,8 +9,8 @@ class User(AbstractUser):
     email = models.EmailField(max_length=255, unique=True)
     first_name = models.CharField(max_length=50)
     last_name =  models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
     updated_at = models.CharField(max_length=50)
-    
+    REQUIRED_FIELDS = ['first_name', 'last_name','username']
+    USERNAME_FIELD = "email"
     objects = CustomUserManager
-    REQUIRED_FIELDS = ['first_name', 'last_name','email']
-    
--- users/utils.py
+++ users/utils.py
@@ -3,19 +3,16 @@ from datetime import datetime
 
 #sobrescrever base de criação
 class CustomUserManager(BaseUserManager):
-    def create_super_user(self,email,password,updated_at,**extra_fields):
+    def create_super_user(self,email,password,**extra_fields):
         if not email:
             raise ValueError('E-mail must be passed')
         
         email = self.normalize_email(email)
         
-        updated_at = updated_at
-        
-        user = self.model(email=email, is_staff=True, is_superuser=True, **extra_fields)
+        user = self.model(email=email, is_staff=True, is_superuser=True, updated_at=datetime.now(),**extra_fields)
         
         user.set_password(password)
         
         user.save(using=self._db)
         
         return user
-    
