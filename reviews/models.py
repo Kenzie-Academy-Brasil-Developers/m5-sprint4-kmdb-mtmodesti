6a4744631d7da5cc605e8ed735d8b1daa156e675
mtmodesti <marcelomodesti1988@gmail.com>
2022-06-24 11:01:47 -0300

    feat: review choices implemented

--- reviews/models.py
+++ reviews/models.py
@@ -2,11 +2,29 @@
 from pickle import FALSE
 from django.db import models
 
+
+class ReviewChoices(models.TextChoices):
+    MUST_WATCH = ("MW","Must Watch")
+    SHOULD_WATCH = ("SW","Should Watch")
+    AVOID_WATCH = ("AW","Avoid Watch")
+    NO_OPINION = ("NO","No Opinion")
+    
+
+
+
 class Review(models.Model):
     stars = models.IntegerField()
     review = models.TextField()
     spoilers = models.BooleanField(default=False)
-    recomendation = models.TextField(max_length=50)
+    
+    
+    recomendation = models.TextField(
+        max_length=50,
+        choices=ReviewChoices.choices,
+        default=ReviewChoices.NO_OPINION
+        )
+    
+    
     
     user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name='user')
     
